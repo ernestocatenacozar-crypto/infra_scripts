61fd8c84195b4dfb0536d4d715f75122abc8a59f
EC2 Bootstrap <ec2@infra.local>
2026-01-29 00:05:04 +0000

    auto-commit EC2

--- update_endpoints.sh
+++ update_endpoints.sh
@@ -3,6 +3,8 @@ set -e
 
 REPO_DIR="/home/ubuntu/work/infra_scripts"
 MD_FILE="README.md"
+CRONICLE_CONF="$REPO_DIR/cronicle/conf/config.json"
+CRONICLE_CONTAINER="cronicle"
 
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
@@ -30,9 +32,21 @@ http://${IP}:7900
 http://${IP}:5002
 EOF
 
+jq --arg url "http://${IP}:3012" '
+  .base_app_url = $url
+  | .custom_live_log_socket_url = $url
+  | .server_comm_use_hostnames = false
+  | .web_socket_use_hostnames = false
+' "$CRONICLE_CONF" > "${CRONICLE_CONF}.tmp"
+
+mv "${CRONICLE_CONF}.tmp" "$CRONICLE_CONF"
+
 git config user.email "ec2@infra.local"
 git config user.name "EC2 Bootstrap"
 
 git add $MD_FILE
 git diff --cached --quiet || git commit -m "Update service endpoints (${IP})"
 git push origin main
+
+
+docker restart "$CRONICLE_CONTAINER"
