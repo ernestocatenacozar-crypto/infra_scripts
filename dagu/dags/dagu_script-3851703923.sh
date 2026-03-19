da0ad0773e1dc92b34366bad6f51df404e0fe608
EC2 Bootstrap <ec2@infra.local>
2026-03-19 11:10:04 +0000

    auto-commit EC2

--- dagu/dags/dagu_script-3851703923.sh
+++ dagu/dags/dagu_script-3851703923.sh
@@ -2,6 +2,6 @@ env HOME=/tmp bash -lc 'set -eu; IMAGE="infra_scripts-test_frontend_build:latest
   echo "Missing image: $IMAGE";
   echo "Build it first with: docker compose --profile build build test_frontend_build";
   exit 1;
-fi; EXTRA_ARGS=""; if [ "1" = "1" ]; then
+fi; EXTRA_ARGS=""; if [ "0" = "1" ]; then
   EXTRA_ARGS="--no-download-images";
 fi; docker run --rm --entrypoint sh -e ORBITA_CITY="Almeria" -v /home/ubuntu/work/frontend_build/test_frontend:/app -v infra_scripts_test_frontend_node_modules:/app/node_modules -v /home/ubuntu/work/frontend_build/test_frontend/dist:/app/dist -v /home/ubuntu/data_storage:/home/ubuntu/data_storage "$IMAGE" scripts/docker_static_entrypoint.sh --city "Almeria" --out "dist" --base-url "https://orbitatest.b-cdn.net/" $EXTRA_ARGS'
