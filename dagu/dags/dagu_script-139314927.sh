974eecff3370516b48bed3389063c4cff85be62f
EC2 Bootstrap <ec2@infra.local>
2026-03-19 11:45:04 +0000

    auto-commit EC2

--- dagu/dags/dagu_script-139314927.sh
+++
@@ -1,7 +0,0 @@
-echo "[upload_to_bunny] Ejecutando docker run..."; docker run --rm --entrypoint sh \
-  -e BUNNY_STORAGE_ZONE="orbitastorage" \
-  -e BUNNY_STORAGE_API_KEY="a5ebdb10-e74d-45d6-8934617053f9-5f56-4b14" \
-  -e CITY_SLUG="$CITY_SLUG" \
-  -v /home/ubuntu/work/frontend_build/test_frontend/dist:/app/dist \
-  infra_scripts-test_frontend:latest \
-  -c "set -eu; cd /app/dist/$CITY_SLUG; lftp -u \"orbitastorage,a5ebdb10-e74d-45d6-8934617053f9-5f56-4b14\" storage.bunnycdn.com -e \"cd $CITY_SLUG; mirror -R --parallel=25 . .; bye\""
